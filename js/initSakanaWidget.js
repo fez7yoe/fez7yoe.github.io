0b8b29d29f115ce09452d70f132b0805233fcaba
fez7yoe <fez7yoe@gmail.com>
2023-09-02 22:32:19 +0800

    清空

--- js/initSakanaWidget.js
+++
@@ -1,4 +0,0 @@
-function initSakanaWidget() {
-new SakanaWidget({ character: 'takina', controls: false }).setState({ i: 0.0005, d: 1 }).mount('#sakana-widget-takina');
-new SakanaWidget({ character: 'chisato', controls: false }).setState({ i: 0.0005, d: 1 }).mount('#sakana-widget-chisato');
-}
